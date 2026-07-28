d1dff3010caa95dec22fb6d47daf9056e20d207e
nicholas-apolinario03 <nicholas.apolinario@outlook.com>
2026-07-28 14:17:28 -0300

    corrigir import da rota whatsapp

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,7 @@ import bcrypt from "bcrypt";
 import { gerartoken } from "./service/jwt";
 import { auth } from "./middleware/auth";
 
-import whatsappRoutes from "./routes/WhatsappRoutes";
+import whatsappRoutes from "./routes/whatsappRoutes";
 
 const app = express();
 
